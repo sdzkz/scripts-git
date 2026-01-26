9cb7143957c86bfae0dec0ffbe3b48e2b5b1211b
sdzkz <sdzkz@users.noreply.github.com>
2026-01-25 20:50:15 -0600

    Venv stuff

--- activate_venv.sh
+++ activate_venv.sh
@@ -1,20 +1,9 @@
 # Run in python project root to activate venv
 
+env_dir="venv"
 
-# Find the first directory ending with '-env' in the current working directory
-env_dir=$(find . -maxdepth 1 -type d -name "*-env" -print -quit)
-
-if [ -n "$env_dir" ]; then
-    # Get the absolute path of the found directory
-    absolute_env_dir=$(cd "$env_dir" && pwd -P)
-    
-    # Check if the activation script exists
-    if [ -f "$absolute_env_dir/bin/activate" ]; then
-        # Source the activation script
-        source "$absolute_env_dir/bin/activate"
-    else
-        echo "Activation script not found at $absolute_env_dir/bin/activate"
-    fi
+if [ -f "$env_dir/bin/activate" ]; then
+    source "$env_dir/bin/activate"
 else
-    echo "No directory ending with '-env' found in the current working directory."
+    echo "Activation script not found at $env_dir/bin/activate"
 fi
--- mkvenv.sh
+++ mkvenv.sh
@@ -1,11 +1,4 @@
 #!/bin/zsh
 
-# Create python venv 
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 ENV_NAME"
-    exit 1
-fi
-
-python3 -m venv "$1"
-echo "export PYTHONPATH=\$PWD" >> "$1/bin/activate"
+python3 -m venv venv
+echo "export PYTHONPATH=\$PWD" >> "venv/bin/activate"
